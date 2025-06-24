a9ce9c9f55d750c5b42ac4d75d8a3ef42ea5a948
SilverStr1ng <rakuyou63@gmail.com>
2025-06-24 18:14:17 +0800

    chore(dep): 添加shadcn-vue组件库

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
+import path from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import cesium from 'vite-plugin-cesium'
+import tailwind from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,11 +14,12 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
-    cesium()
+    cesium(),
+    tailwind()
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
