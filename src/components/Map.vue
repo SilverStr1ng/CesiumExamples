2605074b28e711776202f1a661408000e6366f48
SilverStr1ng <rakuyou63@gmail.com>
2025-06-30 15:32:06 +0800

    chore: 初始化地球配置

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -14,10 +14,25 @@ Cesium.Ion.defaultAccessToken = import.meta.env.VITE_CESIUM_TOKEN
 
 onMounted(async () => {
   const viewer = new Cesium.Viewer("container", {
-    terrain: Cesium.Terrain.fromWorldTerrain()
+    baseLayerPicker: false,
+    geocoder: false,
+    homeButton: false,
+    sceneModePicker: false,
+    timeline: false,
+    navigationHelpButton: false,
+    fullscreenButton: false,
+    animation: false,
   })
 
   window.__viewer = viewer
+
+  // 添加地形
+  const terrain = await Cesium.CesiumTerrainProvider.fromIonAssetId(1)
+  viewer.terrainProvider = terrain
+
+  // 开启光照
+  viewer.scene.globe.enableLighting = true
+
 })
 </script>
 
